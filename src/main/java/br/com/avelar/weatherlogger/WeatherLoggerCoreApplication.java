e27f7701321597eaf1a5cb456850692c08effd82
Geovanny Avelar <geovanny.avelar@gmail.com>
2017-03-17 23:11:19 -0300

    Configuration files

--- src/main/java/br/com/avelar/weatherlogger/WeatherLoggerCoreApplication.java
+++ src/main/java/br/com/avelar/weatherlogger/WeatherLoggerCoreApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherLoggerCoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherLoggerCoreApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherLoggerCoreApplication.class, args);
+  }
 }
