33b3f90c472f7305e4580550b9ebe18e16e7fa65
Geovanny Avelar <geovanny.avelar@gmail.com>
2017-03-17 21:40:22 -0300

    Initial commit

---
+++ src/main/java/br/com/avelar/weatherlogger/WeatherLoggerCoreApplication.java
@@ -0,0 +1,12 @@
+package br.com.avelar.weatherlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherLoggerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherLoggerCoreApplication.class, args);
+	}
+}
