2dd966da4dab7c5bee9428fbdab89ad4512a120c
Geovanny Avelar <geovanny.avelar@gmail.com>
2017-03-19 10:54:36 -0300

    Access control list system completed

--- weatherLogger-DDL.sql
+++ weatherLogger-DDL.sql
@@ -5,7 +5,7 @@ create table if not exists weather_data (
   relative_humidity  double default null,
   temperature  double not null,
   temperature_unit  int(11) not null,
-  PRIMARY KEY (id)
+  primary key (id)
 );
 
 create table if not exists user (
@@ -35,7 +35,7 @@ create table if not exists oauth_access_token (
   token blob,  
   authentication_id varchar(256) default null,  
   user_name varchar(256) default null,  
-  client_id varchar(256) default null,  
+  client_id varchar(256) default null,
   authentication blob,  
   refresh_token varchar(256) default null  
 ); 
@@ -44,6 +44,46 @@ create table if not exists oauth_refresh_token (
   token_id varchar(256) default null,  
   token blob,
   authentication blob  
+);
+
+create table if not exists acl_sid (
+	id bigint unsigned not null auto_increment primary key,
+	principal boolean not null,
+	sid VARCHAR(100) not null,
+	unique key unique_acl_sid (sid, principal)
+);
+
+create table if not exists acl_class (
+	id bigint unsigned not null auto_increment primary key,
+	class VARCHAR(100) not null,
+	unique key uk_acl_class (class)
+);
+
+create table if not exists acl_object_identity (
+	id bigint unsigned not null auto_increment primary key,
+	object_id_class bigint unsigned not null,
+	object_id_identity bigint not null,
+	parent_object bigint unsigned,
+	owner_sid bigint unsigned,
+	entries_inheriting boolean not null,
+	unique key uk_acl_object_identity (object_id_class, object_id_identity),
+	constraint fk_acl_object_identity_parent foreign key (parent_object) references acl_object_identity (id),
+	constraint fk_acl_object_identity_class foreign key (object_id_class) references acl_class (id),
+	constraint fk_acl_object_identity_owner foreign key (owner_sid) references acl_sid (id)
+);
+
+create table if not exists acl_entry (
+	id bigint unsigned not null auto_increment primary key,
+	acl_object_identity bigint unsigned not null,
+	ace_order INTEGER not null,
+	sid bigint unsigned not null,
+	mask INTEGER unsigned not null,
+	granting boolean not null,
+	audit_success boolean not null,
+	audit_failure boolean not null,
+	unique key unique_acl_entry (acl_object_identity, ace_order),
+	constraint fk_acl_entry_object foreign key (acl_object_identity) references acl_object_identity (id),
+	constraint fk_acl_entry_acl foreign key (sid) references acl_sid (id)
 );
 
 # Password is root
